b1491c68fafa8a4721e559cbe0f814b8f281385f
MohamedStack <dammakahmed01@gmail.com>
2026-07-31 08:36:34 +0100

    added some features

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,7 @@
 fun main(args : Array<String>){
     val source = "6 + 4"
     val tokens :List<Token> = Lexer(source).scanTokens()
-    println(tokens)
+    for (token in tokens){
+        println(token)
+    }
 }
